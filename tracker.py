ab4bfc516e81a67913fdc6acfe6a2ac64ca2dd91
gia-lexa <103389320+gia-lexa@users.noreply.github.com>
2024-10-14 15:48:14 -0700

    Adds import packages

--- tracker.py
+++ tracker.py
@@ -1,3 +1,7 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import datetime
 
 # instantiate var to store stock data
 # fetch the latest stock price
